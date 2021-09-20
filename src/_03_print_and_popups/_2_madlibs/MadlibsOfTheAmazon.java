6dd60df55fa49998e2e27dfd6443f64e42f19d88
student <student@ip-172-31-51-108.us-west-2.compute.internal>
2021-09-20 16:59:21 -0700

    Madlibs

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -9,15 +9,15 @@ public class MadlibsOfTheAmazon {
 		String prompt =  "If you find yourself having to cross a piranha-infested river, here's how to do it...";
 		
 		// Get the player to enter an adjective
-
+String adjective = JOptionPane.showInputDialog("Enter an Adjective");
 		// Get the player to enter a type of liquid
-
+String liquid = JOptionPane.showInputDialog("Enter a liquid");
 		// Get the player to enter a body part
-
+String bodypart = JOptionPane.showInputDialog("Enter a body part");
 		// Get the player to enter a verb
-
+String verb = JOptionPane.showInputDialog("Enter a verb");
 		// Get the player to enter a place
-
+String place = JOptionPane.showInputDialog("Enter a place");
 		// The story below has has been written as a group of Strings joined together by + signs.
 		// The story contains place holders, indicated by [** **] which you need to replace with
 		// the values entered by the player.
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,7 +8,13 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+String answer = JOptionPane.showInputDialog("Do you know how to write code?");
+if(answer.equalsIgnoreCase("Yes")) {
+	JOptionPane.showMessageDialog(null, "You will rule the world");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Sign up for classes at the Leaugue");
+}
 		// 2. If they say "yes", tell them they will rule the world.
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
