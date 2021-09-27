4c7c7e10dc8cba4ece56b375b40f0f61191af9a4
student <student@ip-172-31-54-60.us-west-2.compute.internal>
2021-09-27 16:59:29 -0700

    Dragon fight

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,34 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int count = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String answer = JOptionPane.showInputDialog("How many days are there in a year?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if(answer.equalsIgnoreCase("365")) {
 				// 4.  if the user's answer was correct, add one to their score 
-		
+		count += 1;}
+				else {
+					JOptionPane.showMessageDialog(null, "The correct answer is 365");
+					count -= 1;
+				}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		answer = JOptionPane.showInputDialog("What is the first color in the rainbow?");
+		if(answer.equalsIgnoreCase("Red")) {
+		count += 1;}
+		else {
+			JOptionPane.showMessageDialog(null, "incorrect. The answer is red.");
+			count -= 1;
+		}
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "congratulations, you've scored " +count);
 	}
 }
--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -16,17 +16,17 @@ public class DragonFight {
 		// 1. Create some variables to hold health levels
 		
 			// playerHealth to store your health - set it equal to 100
-	
+	int playerhealth = 100;
 			// dragonHealth to store the dragon's health - set it equal to 100
-
+int dragonhealth = 100;
 		// 2. Create some variables to hold the attack strengths. These will be given different values later. 
 		
 			// playerAttack to store the damage the player's attack will do - set it equal
 			// to 0 for now.
-	
+	int playerattack = 0;
 			// dragonAttack to store the damage the dragon's attack will do - set it equal
 			// to 0 for now.
-
+int dragonattack = 0;
 		
 		//  This while statement will cause the game attack code to repeat
 		while (true) {
@@ -35,24 +35,28 @@ public class DragonFight {
 
 				// 3. Ask the player in a pop-up if they want to attack the dragon with a yell
 				// or a kick
-	
+	String answer = JOptionPane.showInputDialog("Do you want to attack the dragon with a yell or a kick?");
 				// 4. If they typed in "yell":
-	
+	if(answer.equalsIgnoreCase("yell")) {
+		playerattack=ran.nextInt(10);
+	}
 					  // -- Find a random number between 0 and 10 and store it in playerAttack. Use
 					  // ran.nextInt(10)
 			
 				// 5. If they typed in "kick":
-	
+	if(answer.equalsIgnoreCase("Kick")) {
+		playerattack=ran.nextInt(25);
+	}
 					  // -- Find a random number between 0 and 25 and store it in playerAttack.
 		
 				// 6. Subtract the player attack value from the dragon's health
-
+dragonhealth -= playerattack;
 			// THE DRAGON RETALIATES
 
 				// 7. Find a random number between 0 and 35 and store it in dragonAttack
-	
+	dragonattack=ran.nextInt(35);
 				// 8. Subtract the dragon attack value from the player's health
-
+playerhealth -= dragonattack;
 			// ASSESS THE DAMAGE
 
 				// 9. If the player's health is less than or equal to 0, the game is over,
